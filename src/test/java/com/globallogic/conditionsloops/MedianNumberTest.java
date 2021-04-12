eaa8b0ec9728fc3c52ad8e37255681db83d3a79b
Oleksandr Navka <oleksandr.navka@globallogic.com>
2021-04-12 15:50:38 +0300

    fixed tests for median number task

--- src/test/java/com/globallogic/conditionsloops/MedianNumberTest.java
+++ src/test/java/com/globallogic/conditionsloops/MedianNumberTest.java
@@ -10,19 +10,19 @@ class MedianNumberTest {
 
     @Test
     void medianNumberFirstNumber() {
-        int actual = unit.medianNumber(4, 1, 8);
+        int actual = unit.medianNumber(4, 1, 28);
         assertEquals(4, actual);
     }
 
     @Test
     void medianNumberSecondNumber() {
-        int actual = unit.medianNumber(2, 5, 8);
+        int actual = unit.medianNumber(2, 5, 28);
         assertEquals(5, actual);
     }
 
     @Test
     void medianNumberThirdNumber() {
-        int actual = unit.medianNumber(2, 5, 3);
+        int actual = unit.medianNumber(2, 55, 3);
         assertEquals(3, actual);
     }
 }
